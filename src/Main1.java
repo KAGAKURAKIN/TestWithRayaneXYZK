1b9d4c97ff26ca0095bc99753aaf126525900c85
Rayboi753 <tvheahray753@gmail.com>
2026-01-31 21:24:03 +0100

    Describe Changes

--- src/Main1.java
+++ src/Main1.java
@@ -1,5 +1,12 @@
 public class Main1 {
     static void main(String[] args) {
         System.out.println("Hello Yassine");
+        float kids_touched = 753;
+        float kids_saved = 431;
+        System.out.println(kids_touched * kids_saved + " " + kids_touched + " " + kids_saved);
+        boolean yamagawa_danino = true;
+        boolean yamagawa_cisco = false;
+        System.out.println(yamagawa_cisco);
+        System.out.println(yamagawa_danino);
     }
 }
