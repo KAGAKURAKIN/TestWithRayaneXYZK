cd2f37450f23e245c77507861acb566575f0a41f
Kara <Karaneedshelp@gmail.com>
2026-01-31 21:45:39 +0100

    Describe changes

--- src/Main2.java
+++ src/Main2.java
@@ -1,8 +1,15 @@
 public class Main2 {
     static void main(String[] args) {
         System.out.println("Hello Rayane");
-        int age = 25;
+        int age = 5;
         double SpecificAge = 25.9;
         System.out.println(SpecificAge + " " + age);
+        char amogus = 'ඞ';
+        System.out.println(amogus);
+        if (age >= 18) {
+    System.out.println("You are an adult");
+        } else {
+        System.out.println("You are an child");
+        }
     }
 }
