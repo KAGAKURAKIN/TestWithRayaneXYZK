eac38f0d2f7c81092ca1ddeccba2eb03573fe22d
Kara <Karaneedshelp@gmail.com>
2026-02-01 22:43:27 +0100

    Describe Changes

--- src/Main2.java
+++ src/Main2.java
@@ -1,34 +1,34 @@
 void main() {
     Scanner sc = new Scanner(System.in);
+    String name, password, ans;
+    int age;
 
-    System.out.println("Please enter your username: ");
-    String username = sc.nextLine();
+    do {
+        System.out.println("What is your name? ");
+        name = sc.nextLine();
 
-    System.out.println("Please enter your password: ");
-    String password = sc.nextLine();
-
-    System.out.println("Please enter your age: ");
-    int age = Integer.parseInt(sc.nextLine());
-
-    System.out.print("Is this information correct? (yes/no): ");
-    String ans = sc.nextLine();
-
-    if (ans.equalsIgnoreCase("yes")) {
-        System.out.println("Your name is " + username + " and you're " + age + " years old.");
-    } else {
-        System.out.println("Re-enter your information.");
-
-        System.out.println("Please enter your username: ");
-        username = sc.nextLine();
-
-        System.out.println("Please enter your password: ");
-        password = sc.nextLine();
-
-        System.out.println("Please enter your age: ");
+        System.out.println("What is your age? ");
         age = Integer.parseInt(sc.nextLine());
 
-        System.out.println("Your name is " + username + " and you're " + age + " years old.");
-    }
+        if (age < 13) {
+            System.out.println("you're too young to use this program");
+            System.exit(0);
+        }
+
+        System.out.println("Choose a password: ");
+        password = sc.nextLine();
+
+        System.out.println("Are you sure? (yes/no) ");
+        ans = sc.nextLine();
+
+        if (!ans.equalsIgnoreCase("yes")) {
+            IO.println("Re-enter the info please.\n");
+        }
+
+    } while (!ans.equalsIgnoreCase("yes"));
+
+    System.out.println("You are " + name + " And You are " + age + " Years old.");
+
 
     sc.close();
 }
