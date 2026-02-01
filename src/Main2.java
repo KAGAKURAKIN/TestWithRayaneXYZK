f0f97830212fd3f3a51079ba684289e14beffa50
Kara <Karaneedshelp@gmail.com>
2026-02-01 20:40:17 +0100

    Describe Changes

--- src/Main2.java
+++ src/Main2.java
@@ -1,8 +1,34 @@
-void main() throws InterruptedException {
+void main() {
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("Please enter your username: ");
+    String username = sc.nextLine();
 
+    System.out.println("Please enter your password: ");
+    String password = sc.nextLine();
 
+    System.out.println("Please enter your age: ");
+    int age = Integer.parseInt(sc.nextLine());
 
+    System.out.print("Is this information correct? (yes/no): ");
+    String ans = sc.nextLine();
 
+    if (ans.equalsIgnoreCase("yes")) {
+        System.out.println("Your name is " + username + " and you're " + age + " years old.");
+    } else {
+        System.out.println("Re-enter your information.");
 
+        System.out.println("Please enter your username: ");
+        username = sc.nextLine();
+
+        System.out.println("Please enter your password: ");
+        password = sc.nextLine();
+
+        System.out.println("Please enter your age: ");
+        age = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Your name is " + username + " and you're " + age + " years old.");
+    }
+
+    sc.close();
 }
