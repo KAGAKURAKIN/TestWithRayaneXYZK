6fad954e5cabf8bf43514e3728eaa335346c7081
Kara <Karaneedshelp@gmail.com>
2026-02-01 18:37:29 +0100

    Describe Changes

--- src/Main1.java
+++ src/Main1.java
@@ -1,12 +1,10 @@
-public class Main1 {
-    static void main(String[] args) {
-        System.out.println("Hello Yassine");
-        float kids_touched = 753;
-        float kids_saved = 431;
-        System.out.println(kids_touched * kids_saved + " " + kids_touched + " " + kids_saved);
-        boolean yamagawa_danino = true;
-        boolean yamagawa_cisco = false;
-        System.out.println(yamagawa_cisco);
-        System.out.println(yamagawa_danino);
-    }
+void main() {
+    IO.println("Hello Yassine");
+    float kids_touched = 753;
+    float kids_saved = 431;
+    IO.println(kids_touched * kids_saved + " " + kids_touched + " " + kids_saved);
+    boolean yamagawa_danino = true;
+    boolean yamagawa_cisco = false;
+    IO.println(yamagawa_cisco);
+    IO.println(yamagawa_danino);
 }
--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,13 @@
-public class Main2 {
-    static void main(String[] args) {
-     String Name = "Yassine";
-     System.out.println(Name);
-    }
+void main() throws InterruptedException {
+    int fave_num = 4;
+
+    if (fave_num == 4) {
+        while (fave_num < 100) {
+            System.out.println(fave_num);
+            fave_num += 4;
+            Thread.sleep(400);
+            }
+        } else  {
+        System.out.println("Fave number is not 4");
+        }
 }
