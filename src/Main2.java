ef861b46ebc5da6980abadb1eebb9d578d03a504
Kara <Karaneedshelp@gmail.com>
2026-02-01 19:00:43 +0100

    Describe Changes

--- src/Main2.java
+++ src/Main2.java
@@ -1,13 +1,8 @@
 void main() throws InterruptedException {
-    int fave_num = 4;
 
-    if (fave_num == 4) {
-        while (fave_num < 100) {
-            System.out.println(fave_num);
-            fave_num += 4;
-            Thread.sleep(400);
-            }
-        } else  {
-        System.out.println("Fave number is not 4");
-        }
+
+
+
+
+
 }
